68d120675bf25e81f7f9a76cf9faa83337654f2d
Ming Yang <minos.future@gmail.com>
2025-09-10 23:39:20 -0700

    add interface

--- hopper/flash.h
+++ hopper/flash.h
@@ -161,6 +161,10 @@ struct Flash_fwd_params : public Qkv_params {
 
     // The S extra matrix, (num_heads)
     void *__restrict__ s_aux_ptr;
+
+    // DCP (Decoding Context Parallelism) parameters
+    int cp_world_size;
+    int cp_rank;
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
